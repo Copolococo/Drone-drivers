d5a459c1bc245c15d3b532858cbebea11cfbce4a
Pedro Martins <pedro.miguel.martins9@gmail.com>
2020-11-20 15:16:22 +0000

    added functions to get and set bytes

--- driver.cpp
+++ driver.cpp
@@ -1,106 +1,121 @@
 #include "driver.h"
 
 /**
- *	Calculate the checksum of the packet
+ *  Calculate the checksum of the packet
  */
 void Drone::calcChecksum(void)
 {
-	buffer[6] = 0;
-	for (int i = 1; i < 6; i++)
-		buffer[6] ^= buffer[i];
+    buffer[6] = 0;
+    for (int i = 1; i < 6; i++)
+        buffer[6] ^= buffer[i];
 
 }
 
+char Drone::_getByte(enum _byteName index)
+{
+    bufferMutex.lock();
+    char res = buffer[index];
+    bufferMutex.unlock();
+    return res;
+}
+
+void Drone::_setByte(enum _byteName index, char value)
+{
+    bufferMutex.lock();
+    buffer[index] = value;
+    bufferMutex.unlock();
+}
+
 /**
- *	Thread that runs for the duration of the program, keeps the communication
- *	going and sends the packets periodically
+ *  Thread that runs for the duration of the program, keeps the communication
+ *  going and sends the packets periodically
  */
 void Drone::commLoop(void)
 {
-	while(true)
-	{
-		/* Send packet */
-		bufferMutex.lock();
-		sendto(sockfd, (const char *) buffer, CTRL_PACKET_LEN, MSG_CONFIRM,
-				(const sockaddr *) &servaddr, sizeof(servaddr));
+    while(true)
+    {
+        /* Send packet */
+        bufferMutex.lock();
+        sendto(sockfd, (const char *) buffer, CTRL_PACKET_LEN, MSG_CONFIRM,
+                (const sockaddr *) &servaddr, sizeof(servaddr));
 
-		bufferMutex.unlock();
+        bufferMutex.unlock();
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-	}
+    }
 }
 
 Drone::Drone(void)
 {
-	/* Configure Socket */
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	memset(&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family			= AF_INET;
-	servaddr.sin_port			= htons(UDP_COM_PORT);
-	servaddr.sin_addr.s_addr	= inet_addr(IP_ADDR);
-	
-	buffer[0] = START_BYTE;
-	buffer[7] = END_BYTE;
+    /* Configure Socket */
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family         = AF_INET;
+    servaddr.sin_port           = htons(UDP_COM_PORT);
+    servaddr.sin_addr.s_addr    = inet_addr(IP_ADDR);
+    
+    buffer[0] = START_BYTE;
+    buffer[7] = END_BYTE;
 
-	on();
-	commThread = std::thread(&Drone::commLoop, this);
+    on();
+    commThread = std::thread(&Drone::commLoop, this);
 }
 
 
 Drone::~Drone(void)
 {
-	commThread.join();
-	stop();
-	off();
-	close(sockfd);
+    commThread.join();
+    stop();
+    off();
+    close(sockfd);
 }
 
 void Drone::on(void)
 {
-	bufferMutex.lock();
+    bufferMutex.lock();
 
-	buffer[1] = 0x81;
-	buffer[2] = 0x84;
-	buffer[3] = 0x80;
-	buffer[4] = 0x84;
-	buffer[5] = CTRL_LAND;
-	buffer[6] = 0x01;
+    buffer[1] = 0x81;
+    buffer[2] = 0x84;
+    buffer[3] = 0x80;
+    buffer[4] = 0x84;
+    buffer[5] = CTRL_LAND;
+    buffer[6] = 0x01;
 
-	bufferMutex.unlock();
+    bufferMutex.unlock();
 }
 
 void Drone::off(void)
 {
-	bufferMutex.lock();
-	buffer[5] = CTRL_LAND;
-	bufferMutex.unlock();
+    bufferMutex.lock();
+    buffer[5] = CTRL_LAND;
+    bufferMutex.unlock();
 }
 
 void Drone::stop(void)
 {
-	bufferMutex.lock();
-	buffer[5] = CTRL_STOP;
-	bufferMutex.unlock();
+    bufferMutex.lock();
+    buffer[5] = CTRL_STOP;
+    bufferMutex.unlock();
 }
 
 void Drone::launch(void)
 {
-	bufferMutex.lock();
-	buffer[1] = 0x81;
-	buffer[2] = 0x84;
-	buffer[3] = 0x80;
-	buffer[4] = 0x84;
-	buffer[5] = CTRL_LAUNCH;
-	buffer[6] = 0x00;
-	bufferMutex.unlock();
+    bufferMutex.lock();
+    buffer[1] = 0x81;
+    buffer[2] = 0x84;
+    buffer[3] = 0x80;
+    buffer[4] = 0x84;
+    buffer[5] = CTRL_LAUNCH;
+    buffer[6] = 0x00;
+    bufferMutex.unlock();
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-	
-	bufferMutex.lock();
-	buffer[5] = CTRL_LAND;
-	buffer[6] = 0x01;
-	bufferMutex.unlock();
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    
+    bufferMutex.lock();
+    buffer[5] = CTRL_LAND;
+    buffer[6] = 0x01;
+    bufferMutex.unlock();
 }
 
 /**
@@ -108,13 +123,13 @@ void Drone::launch(void)
  */
 void Drone::setState(char yaw, char surge, char heave, char sway)
 {
-	bufferMutex.lock();
-	buffer[1] = yaw;
-	buffer[2] = surge;
-	buffer[3] = heave;
-	buffer[4] = sway;
+    bufferMutex.lock();
+    buffer[1] = yaw;
+    buffer[2] = surge;
+    buffer[3] = heave;
+    buffer[4] = sway;
 
-	calcChecksum();
-	bufferMutex.unlock();
+    calcChecksum();
+    bufferMutex.unlock();
 }
 
--- driver.h
+++ driver.h
@@ -1,47 +1,60 @@
-#include <thread>		// multithreading
-#include <mutex>		// threadsafe variables
+#include <thread>       // multithreading
+#include <mutex>        // threadsafe variables
 #include <cstdlib>
 #include <iostream>
-#include <chrono>		// time keeping
-#include <sys/socket.h>	// websocket
-#include <sys/types.h>	// websocket types
-#include <unistd.h>		// close(fd)
-#include <netinet/in.h>	// inet_addr()
-#include <arpa/inet.h>	// inet_addr()
-#include <cstring>		// memset()
+#include <chrono>       // time keeping
+#include <sys/socket.h> // websocket
+#include <sys/types.h>  // websocket types
+#include <unistd.h>     // close(fd)
+#include <netinet/in.h> // inet_addr()
+#include <arpa/inet.h>  // inet_addr()
+#include <cstring>      // memset()
 
-#define IP_ADDR			"192.168.0.1"
-#define UDP_COM_PORT	50000
-#define UDP_VIDEO_PORT	40000
+#define IP_ADDR         "192.168.0.1"
+#define UDP_COM_PORT    50000
+#define UDP_VIDEO_PORT  40000
 
-#define START_BYTE		0x66
-#define END_BYTE		0x99
+#define START_BYTE      0x66
+#define END_BYTE        0x99
 
-#define CTRL_PACKET_LEN	8
-#define CTRL_LAND		0x00
-#define CTRL_LAUNCH		0x01
-#define CTRL_STOP		0x04
-#define CTRL_FLIP		0x08
-#define CTRL_RSTGYRO	0x80
+#define CTRL_PACKET_LEN 8
+#define CTRL_LAND       0x00
+#define CTRL_LAUNCH     0x01
+#define CTRL_STOP       0x04
+#define CTRL_FLIP       0x08
+#define CTRL_RSTGYRO    0x80
+
+enum _byteName{Start, Yaw, Surge, Heave, Sway, Cmd, Checksum, Stop};
 
 class Drone
 {
-	private:
-		struct sockaddr_in servaddr;
-		int sockfd;
-		char buffer[8];
-		void calcChecksum(void);
-		void commLoop(void);
-		std::thread commThread;
-		std::mutex bufferMutex;
+    private:
+        struct sockaddr_in servaddr;
+        int sockfd;
+        char buffer[8];
+        void calcChecksum(void);
+        void commLoop(void);
+        std::thread commThread;
+        std::mutex bufferMutex;
+        char _getByte(enum _byteName byteName);
+        void _setByte(enum _byteName byteName, char value);
 
-	public:
-		Drone(void);
-		~Drone(void);
-		void on(void);
-		void off(void);
-		void stop(void);
-		void launch(void);
-		void setState(char yaw, char surge, char heave, char sway);
-		char getYaw(void) {return buffer[1];};
+    public:
+        Drone(void);
+        ~Drone(void);
+        void on(void);
+        void off(void);
+        void stop(void);
+        void launch(void);
+        void setState(char yaw, char surge, char heave, char sway);
+        char getYaw(void) { return _getByte(Yaw); };
+        char getSurge(void) { return _getByte(Surge); };
+        char getHeave(void) { return _getByte(Heave); };
+        char getSway(void) { return _getByte(Sway); };
+        char getCmd(void) { return _getByte(Cmd); };
+        void setYaw(char value) { _setByte(Yaw, value); calcChecksum(); };
+        void setSurge(char value){ _setByte(Surge, value); calcChecksum(); };
+        void setSway(char value){ _setByte(Sway, value); calcChecksum(); };
+        void setCmd(char value){ _setByte(Cmd, value); calcChecksum(); };
+
 };
